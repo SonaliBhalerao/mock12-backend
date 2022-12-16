0e625ac987e81299322625550017ccc5cb12eb5c
SonaliBhalerao <sonalibhalerao120@gmail.com>
2022-12-16 22:43:41 +0530

    backend
    backend

---
+++ Models/User.model.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+mongoose.set('strictQuery', true);
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    email:String,
+    password: String
+})
+
+const UserModel = mongoose.model("users", userSchema);
+
+
+module.exports = {
+    UserModel
+}
---
+++ Routes/User.Route.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { UserModel }= require("../Models/User.model");
+
+const userRoutes = express.Router();
+
+userRoutes.post("/signup",async(req, res)=>{
+    const { name, email, password } = req.body;
+    await bcrypt.hash(password, 8, function(err, hash){
+        if(err){
+            return res.send("Signup failed")
+        }
+        const user = new UserModel({name, email, password:hash})
+        user.save();
+        return res.send("Signup Successful!");
+    })
+});
+
+userRoutes.post("/login", async(req, res)=>{
+    const {email, password} = req.body;
+
+    const user = await UserModel.findOne({email});
+    const new_password = user.password;
+
+    await bcrypt.compare(password, new_password, function(err, result){
+        if(err){
+            return res.send("Please Login");
+        }
+        if(result){
+            const token = jwt.sign(
+                {email:user.email, _id:user._id},
+                "secret"
+            );
+            if(user.length === 0){
+                return res.send("Invalid Credentials");
+            }
+            return res.send({message:"Login Successful!", token: token});
+        }
+        else{
+            return res.send("Invalid Credentials");
+        }
+    })
+})
+
+
+module.exports = userRoutes;
